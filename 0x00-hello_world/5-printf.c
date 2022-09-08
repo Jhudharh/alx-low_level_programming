6b5420ad55f93cfbf07fed3cfc157fc136865dcf
Jhudharh <judahitah@gmail.com>
2022-09-08 09:32:45 -0700

    0x00-hello_world

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+* main - prints the phrase with power grammar,
+* but the outmoce is a piece of art,
+* Return: 0 if it exits properly, non-zero otherwise
+*/
+int main(void)
+{
+       	printf("With proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
